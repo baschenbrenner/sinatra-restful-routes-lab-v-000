0b5079f214fcc531d3563e7a9198cdbd8f7802cc
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-19 13:21:43 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171119130924) do
+ActiveRecord::Schema.define(version: 20171119132013) do
 
   create_table "recipes", force: :cascade do |t|
-    t.string  "name"
-    t.text    "ingredients"
-    t.integer "cook_time"
+    t.string "name"
+    t.text   "ingredients"
+    t.string "cook_time"
   end
 
 end
