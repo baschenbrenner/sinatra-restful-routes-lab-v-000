b2db38ffc0a39f357c6b6bac00172d167a2187ab
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-19 13:52:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,8 +37,15 @@ class ApplicationController < Sinatra::Base
     redirect '/recipes'
   end
 
-  get '/recipes/:id/edit'
+  get '/recipes/:id/edit' do
     @recipe = Recipe.find(params[:id])
     erb :edit
+  end
 
+  post '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+  end
 end
